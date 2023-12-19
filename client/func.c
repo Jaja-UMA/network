133bbb521910c8dd1582d6bbe41ffb279b115f78
Kubra Sali <westakr24@gmail.com>
2023-12-19 15:21:54 +0900

    [fix]connectserver用にプログラム修正

--- client/func.c
+++ client/func.c
@@ -18,5 +18,21 @@ int myprint(const char *text){
 
 int connectServer(const char *host,const char *path,const char *port)
 {
+    int socket;
+
+    if(getaddrinfo(host,port,&hints,&res) !=0){
+        printf("error:getaddrinfoargv[0]= %s\n",argv[1]);
+    }
+    socket = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    if(socket == -1){
+        fprintf(stderr,"socket Error\n");
+        return 0;
+    }
+    if(connect(socket,res->ai_addr,res->ai_addrlen) == -1)
+    {
+        fprintf(stderr,"connect\n");
+        return 0;
+    }
+    printf("connection is done\n");
 
 }
