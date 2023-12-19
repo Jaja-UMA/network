cb246ccbe3756e8cb07479b0724c6c8b951e02f2
Kubra Sali <westakr24@gmail.com>
2023-12-19 15:46:43 +0900

    不要部分削除

--- client/client.c
+++ client/client.c
@@ -7,39 +7,30 @@
 //第三引数:ポート番号
 
 int main(int argc,char *argv[]){
+    if(argc!=4 || argc > 4)
+    {
+        fprintf(stderr,"引数の数が足りませんor多い\n第一引数:接続先第二引数:ファイルパス 第三引数:ポート番号\n");
+        return 0;
+    }
+
+
     struct addrinfo hints,*res;
     int s,com_code;
     char buf[BUF_SIZE];
     char send_mes[BUF_SIZE];
-    if(argc!=4)
-    {
-        fprintf(stderr,"引数の数が足りません\n第一引数:接続先第二引数:ファイルパス 第三引数:ポート番号\n");
-        return 0;
-    }
+
     memset(send_mes,0,sizeof(send_mes));
     sprintf(send_mes,"GET /%s\r\n",argv[2]);
-    memset(&hints,0,sizeof(hints));
     memset(buf,0,BUF_SIZE);
+
+    //addrinfoに関する初期化
+    memset(&hints,0,sizeof(hints));
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_family = AF_INET;
 
     printf("argv[1]:%s argv[2]:%s argv[3]:%s\nsend_mes = %s\n",argv[1],argv[2],argv[3],send_mes);
 
-    if(getaddrinfo(argv[1],argv[3],&hints,&res) !=0){
-        printf("error:getaddrinfoargv[0]= %s\n",argv[1]);
-    }
-    s = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-    if(s==-1){
-        fprintf(stderr,"socket Error\n");
-        return 0;
-    }
-    if(connect(s,res->ai_addr,res->ai_addrlen) == -1)
-    {
-        fprintf(stderr,"connect\n");
-        return 0;
-    }
-    printf("connection is done\n");
-
+    s=connectServer(argv[1],argv[3],&hints,res);
     if(send(s,send_mes,BUF_SIZE,0)==-1)
     {
         fprintf(stderr,"sendERROR\n");
