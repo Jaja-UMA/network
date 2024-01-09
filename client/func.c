260c4487f417c0ea7d9a2b1a9def8b14924336bf
Kubra Sali <westakr24@gmail.com>
2024-01-09 13:17:18 +0900

    %を持つ文字列の分岐処理

--- client/func.c
+++ client/func.c
@@ -19,8 +19,8 @@ int next_procec(char *input){
     //printf("%d:next_procec\n",strlen(input));
     if (strcmp(input, "%Q\n") == 0) {
         return 0;
-    }else if(input[0]=="%"){
-        return 0;
+    }else if(input[0]=='%'){
+        return 1;
     } else {
         printf("一致する文字列がありません\n");
         return -1;
